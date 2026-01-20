28560702d99b021b9471dfd352126f34c5a4ad0c
Franklin Fanya <franklinfanya@gmail.com>
2026-01-20 18:27:31 +0100

    fix

--- src/components/dashboard/layout/sidebar/SidebarHeader.jsx
+++ src/components/dashboard/layout/sidebar/SidebarHeader.jsx
@@ -1,14 +1,7 @@
-import { Link } from 'react-router-dom'
-import { AnimatedLogo } from '../../../common'
-
 export default function SidebarHeader() {
-  return (
-    <div className="px-6 py-8 border-b border-gray-200">
-      <Link to="/" className="flex items-center gap-3 hover:opacity-80 transition-opacity">
-        <AnimatedLogo className="h-10 sm:h-12 w-auto" />
-      </Link>
-    </div>
-  )
+  return null
 }
 
 SidebarHeader.propTypes = {}
+
+
