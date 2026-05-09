11cee6a5ce8cd728364a5b11ed92fe5d5c846e5c
Devhubfusionx <jeeyalex7@gmail.com>
2026-05-09 23:57:09 +0100

    chatbot fix

--- src/components/layout/PublicLayout.jsx
+++ src/components/layout/PublicLayout.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import { Navbar } from '../common'
 import Footer from '../common/Footer'
-import StickyChat from '../chat/StickyChat'
 
 export default function PublicLayout() {
     return (
@@ -11,7 +10,6 @@ export default function PublicLayout() {
                 <Outlet />
             </main>
             <Footer />
-            <StickyChat />
         </div>
     )
 }
