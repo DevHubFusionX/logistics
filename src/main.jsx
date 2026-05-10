8dee785443e3f91a7a98c4272714b7c0be390ae8
Devhubfusionx <jeeyalex7@gmail.com>
2026-05-10 08:52:30 +0100

    chatbot fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode, lazy, Suspense } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { HelmetProvider } from 'react-helmet-async'
@@ -6,16 +6,11 @@ import { queryClient } from './lib/queryClient'
 import './index.css'
 import App from './App.jsx'
 
-const ReactQueryDevtools = import.meta.env.DEV
-  ? (await import('@tanstack/react-query-devtools')).ReactQueryDevtools
-  : () => null
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelmetProvider>
       <QueryClientProvider client={queryClient}>
         <App />
-        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </HelmetProvider>
   </StrictMode>,
